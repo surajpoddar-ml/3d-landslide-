f9692f378e004b5a1e0806166bf9acd76754940e
suraj <surajpoddar098@gmail.com>
2026-04-14 00:49:53 +0545

    uploading file

---
+++ Assets/VRoad/Scripts/uk/vroad/Editor/SceneSaveMonitor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace uk.vroad.Editor
+{
+    public interface LSceneSave
+    {
+        void OnWillSaveScene();
+    }
+    
+    /// <summary>
+    /// This Editor class catches user "Save Scene" actions and calls any listeners that are registered
+    /// </summary>
+    public class SceneSaveMonitor: UnityEditor.AssetModificationProcessor
+    {
+        private static readonly HashSet<LSceneSave> listeners = new HashSet<LSceneSave>();
+
+        public static void register(LSceneSave listener)
+        {
+            listeners.Add(listener);
+        }
+        
+        public static string[] OnWillSaveAssets(string[] paths)
+        {
+            if (listeners.Count == 0) return paths;
+            
+            bool sceneSaved = false;
+            foreach (string path in paths)
+            {
+                if (path.EndsWith(".unity"))
+                {
+                    sceneSaved = true;
+                    break;
+                }
+            }
+
+            if (sceneSaved)
+            {
+                foreach (LSceneSave listener in listeners)
+                {
+                    listener.OnWillSaveScene();
+                }
+            }
+            return paths;
+        }
+    }
+}
---
+++ Assets/VRoad/Scripts/uk/vroad/ucm/MeshTools.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using uk.vroad.apk;
+using UnityEditor;
+using UnityEngine;
+
+namespace uk.vroad.ucm
+{
+    /// <summary> A class containing static Utility methods to assist with drawing of Meshes </summary>
+    public class MeshTools: ScriptableObject
+    {
+#if UNITY_EDITOR
+        private static string vroadRootRel = SA.EXPECTED_PKG_LOC;
+        private static bool checkedRoot = false;
+        
+        
+        /// <summary> Return root directory of our package relative to project root (parent of Assets/ ) <br/>
+        /// This is normally Assets/VRoad, but our package might not have been installed there.
+        /// Editor tools such as EditorSceneManager use  paths relative to the Project folder </summary> 
+        public static string VRoadRoot()
+        {
+            if (checkedRoot) return vroadRootRel;
+            checkedRoot = true;
+
+            bool asExpected = false;
+            try
+            {
+                MeshTools env = CreateInstance<MeshTools>();
+                KFile kenvScriptFile = new KFile(AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(env)));
+                //                                 ucm   / vroad  / uk     / Scripts/ VRoad
+                KDir vroadDir = kenvScriptFile.Directory().Parent().Parent().Parent().Parent();
+                string vroadPath = vroadDir.FullPath();
+                string vroadRootRelActual = vroadPath.Substring(vroadPath.IndexOf(SA.ASSETS_DIR));
+
+                if (vroadRootRel.Equals(vroadRootRelActual)) asExpected = true;
+                else vroadRootRel = vroadRootRelActual;
+            }
+            catch (Exception) { asExpected = false; }
+
+            if (!asExpected) Debug.LogWarning(SA.UNEXPECTED_PKG_LOC + SA.EXPECTED_PKG_LOC);
+			
+            return vroadRootRel;
+        }
+#endif
+        
+
+    }
+}
